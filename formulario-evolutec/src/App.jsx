ba318ba688edbf5b5f262d9769d34d5c0dbacdde
Hiarleyy <Marcoshiarley.silva@gmail.com>
2026-02-02 17:28:03 -0300

    refactor: remove Google Tag Manager initialization from App.jsx

--- formulario-evolutec/src/App.jsx
+++ formulario-evolutec/src/App.jsx
@@ -2,21 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import cursos from './cursos.json'
 import { SpeedInsights } from "@vercel/speed-insights/react"
-
-const tagManagerArgs = {
-  gtmId: "G-1P9E2V20X6"
-}
-
-TagManager.initialize(tagManagerArgs)
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-)
-
-
-
 function Modal({ isOpen, onClose }) {
   if (!isOpen) return null;
   return (
